7b4b0415147be57f1eb5332f039db233a07dbd6f
Juan Insua <insuajuancruz@gmail.com>
2023-06-21 22:27:04 -0300

    Descomentado y muestra

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import Logica.Principal.SistemaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        //SistemaPrincipal sistemaPrincipal=new SistemaPrincipal(null);
+        SistemaPrincipal sistemaPrincipal=new SistemaPrincipal(null);
         System.out.println("JSON Usuarios y sus informes");
         ControladoraJson.toJson();
         System.out.println(JsonUtiles.leer("consultorio"));
